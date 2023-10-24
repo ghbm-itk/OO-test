6ff37fe7b945faf7dad6d850404218ce372f2213
ghbm-itk <123645708+ghbm-itk@users.noreply.github.com>
2023-10-24 13:12:58 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+import os
+
+desktop_path = os.path.expanduser("~\\Desktop")
+file_path = os.path.join(desktop_path, "text.txt")
+
+with open(file_path, 'w') as file:
+    file.write(str(datetime.now()))
